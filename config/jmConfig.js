a1c9d8c31cfa7a02165c0fda63151a563a1f906a
quiet <630876811@qq.com>
2023-12-07 21:16:54 +0800

    笑死我了

--- config/jmConfig.js
+++ config/jmConfig.js
@@ -12,16 +12,16 @@
 module.exports = {
   yezi: {
     api: 'http://api.sqhyw.net:90',
-    account: '13263860590',
-    password: 'wang520QQ',
+    account: '',
+    password: '',
     scopeBlack: '192,165,162,167',
     loop: 1,
     operator: 0
   },
   haozhu: {
     api: 'http://api.haozhuma.com',
-    user: 'a614622787', //账号
-    pass: 'lxl123456', //密码
+    user: '', //账号
+    pass: '', //密码
     ascription: '2', //号码类型，留空为不限制，ascription=1只取虚拟，ascription=2只取实卡
     Province: '', //号码省份，可留空
     paragraph: '', //只取号段，留空为不限制 使用 | 符号
